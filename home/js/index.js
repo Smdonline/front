9b2d8365a6e4923adbba2ad070039a4c2f239c94
socia <socianudaniel@gmail.com>
2023-11-08 17:15:00 +0100

    modificato background turno

--- home/js/index.js
+++ home/js/index.js
@@ -5,16 +5,15 @@ $(document).ready(function () {
 
     function init_container() {
         var container=$('<div />',{
-            class: "dashed-grid",
+            class: "dashed-grid"
         }).appendTo('body');
         container.on('dblclick',function () {
             init_shift($(this));
         })
     }
     function init_shift(parinte) {
-        var step=15;
+        var step=10;
         var lungime=step*8;
-        var stanga = 25;
         var turno = $('<div \>',{
             class:'selector'
         });
@@ -26,7 +25,7 @@ $(document).ready(function () {
         lungime = lungime/(4*step);
         turno.draggable({
                 axis: "x",
-                grid: [ step,step],
+                grid: [ step],
                 containment: "parent",
                 drag: function( event, ui ) {
 
@@ -49,7 +48,7 @@ $(document).ready(function () {
                     lungime = parseFloat(ui.size.width/(4*step));
                     $(this).find('.fine').text(parseFloat((ui.position.left)/(4*step) + lungime));
                     $(this).find('.center').text(lungime);
-                },
+                }
 
             });
         turno.appendTo(parinte);
@@ -57,44 +56,8 @@ $(document).ready(function () {
     $('#btn').on( "click", function() {
         init_container();
     } );
+    for(var i=0;i<10;i++){
+        init_container();
+    }
 
-    // var step=15;
-    // var lungime=step*8;
-    // var stanga = 25;
-    // $('.selector').width(lungime);
-    // $('.selector').height(step);
-    // lungime = lungime/(4*step);
-    //
-    // $( ".selector" ).draggable({
-    //     axis: "x,y",
-    //     grid: [ step,step],
-    //     containment: "parent",
-    //     drag: function( event, ui ) {
-    //
-    //
-    //         $('.inizio').text(parseFloat(ui.position.left/(4*step)));
-    //         $('.fine').text(parseFloat((ui.position.left)/(4*step) + lungime));
-    //     },
-    //     create: function( event, ui ) {
-    //         $('.center').text(lungime);
-    //     }
-    // });
-    // $( ".selector" ).resizable({
-    //     handles: "w,e",
-    //     grid: [ step],
-    //     maxWidth: 12*4*step,
-    //     minWidth: step,
-    //     containment: "parent",
-    //     resize: function (event, ui) {
-    //         $('.inizio').text(parseFloat(ui.position.left/(4*step)));
-    //         lungime = parseFloat(ui.size.width/(4*step));
-    //         $('.fine').text(parseFloat((ui.position.left)/(4*step) + lungime));
-    //         $('.center').text(lungime);
-    //     },
-    //     create: function( event, ui ) {
-    //         ui.position.left=stanga;
-    //         lungime = ui.size.width/(4*step);
-    //     }
-    //
-    // });
 });
